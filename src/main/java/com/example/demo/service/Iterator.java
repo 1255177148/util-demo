7d3cae86c3090da9aa57b6373d9797d711dc09be
elvis.he <elvis.he@gaiaworks.cn>
2019-11-18 11:01:39 +0800

    设计模式--迭代器模式2

---
+++ src/main/java/com/example/demo/service/Iterator.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+/**
+ * 抽象迭代器
+ */
+public interface Iterator<E> {
+
+    /**
+     * 是否有下个元素
+     * @return
+     */
+    boolean hasNext();
+
+    /**
+     * 返回下一个元素
+     * @return
+     */
+    E next();
+}
