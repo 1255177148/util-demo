46134278ef3cfa653f3c3aa09d4e76a041fb1eb2
elvis.he <elvis.he@yunlsp.com>
2021-06-01 17:33:08 +0800

    JSON反序列化，String类型的时间戳可以直接转为Date类型

---
+++ src/main/java/com/example/demo/entity/StringToDate.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @Author elvis
+ * @Date 2021/6/1 17:21
+ */
+@Data
+public class StringToDate {
+
+    private Date opTime;
+}
--- src/test/java/com/example/demo/StringDemo.java
+++ src/test/java/com/example/demo/StringDemo.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.demo.entity.StringToDate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
 
 /**
  * @Date 2020/8/7 11:10
@@ -24,5 +29,13 @@ public class StringDemo {
         for (int i = 0; i < strArr.length; i++) {
             System.out.println("i=" + i + "val=" + strArr[i]);
         }
+    }
+
+    @Test
+    public void name() {
+        String str = "{\"manifestType\":\"SEND\",\"manifestId\":333333412591,\"remark\":\"柔柔弱弱退单\",\"opTime\":1622537984364,\"hasChargeBack\":true}";
+        StringToDate toDate = JSONObject.parseObject(str, StringToDate.class);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        System.out.println(format.format(toDate.getOpTime()));
     }
 }
