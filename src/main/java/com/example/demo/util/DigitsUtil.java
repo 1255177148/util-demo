298a92fa4d72ca6ba26573dc47d7f644860cb9ce
elvis.he <elvis.he@yunlsp.com>
2021-07-05 14:03:46 +0800

    给一个数字，以固定的位数返回此数字

---
+++ src/main/java/com/example/demo/util/DigitsUtil.java
@@ -0,0 +1,21 @@
+package com.example.demo.util;
+
+/**
+ * 给一个数字，以固定的位数返回此数字
+ * @Author elvis
+ * @Date 2021/7/5 14:01
+ */
+public class DigitsUtil {
+
+    public static void main(String[] args){
+        int number = 1;
+
+        /**
+         * 0 代表前面补 0
+         * 4 代表数字的位数，或者长度为 4
+         * d 代表参数为正整数
+         */
+        String str = String.format("%04d", number);
+        System.out.println(str);
+    }
+}
